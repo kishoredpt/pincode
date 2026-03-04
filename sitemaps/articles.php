7f703e2459eb56e5cd8db21c35fecc6d1aa31b33
kishoredpt <48178796+kishoredpt@users.noreply.github.com>
2026-03-04 18:00:29 +0530

    Update articles.php

--- sitemaps/articles.php
+++ sitemaps/articles.php
@@ -1,27 +1,23 @@
 <?php
-require_once "../config/db.php";
+header("Content-Type: application/xml; charset=UTF-8");
 
-header("Content-Type: application/xml; charset=utf-8");
+$articles = require __DIR__ . "/../includes/static-articles.php";
+
+$base = "https://pincodelocator.co.in";
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-<?php
-$q = $conn->query("SELECT slug FROM articles");
-
-while ($row = $q->fetch_assoc()) {
-    $slug = urlencode($row['slug']);
-    $url = "https://pincodelocator.co.in/blog-post.php?slug={$slug}";
-?>
-<url>
-<loc><?= htmlspecialchars($url, ENT_QUOTES, 'UTF-8') ?></loc>
-<changefreq>monthly</changefreq>
-<priority>0.6</priority>
-</url>
-<?php endforeach; ?>
-
-<?php } ?>
+<?php if (!empty($articles) && is_array($articles)): ?>
+    <?php foreach ($articles as $article): ?>
+        <url>
+            <loc><?= $base ?>/blog-post.php?slug=<?= htmlspecialchars($article['slug']) ?></loc>
+            <changefreq>monthly</changefreq>
+            <priority>0.7</priority>
+        </url>
+    <?php endforeach; ?>
+<?php endif; ?>
 
 </urlset>
